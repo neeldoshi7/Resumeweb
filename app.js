098cb20162cb95ec3f350fd1bd2a1a9b20c6faac
neeldoshi7 <47111448+neeldoshi7@users.noreply.github.com>
2019-07-27 00:27:50 +0530

    JSadd

---
+++ app.js
@@ -0,0 +1,6 @@
+ //Show Alert Message
+  document.querySelector('.alert').style.display = 'block';
+
+  //Hide Alert Message After Seven Seconds
+  setTimeout(function() {document.querySelector('.alert').style.display = 'none';
+  }, 7000);
