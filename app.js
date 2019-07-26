0e70a918042f1a279ed90cabc5edd8bfa0b2fb29
neeldoshi7 <47111448+neeldoshi7@users.noreply.github.com>
2019-07-27 00:49:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
+ //listen for submit event//
+document.getElementById('form1').addEventListener('submit', formSubmit);
+
+//Submit form
+function formSubmit(e) {
+
+	
+  e.preventDefault();
+  // Get Values from the DOM
+  let fname = document.querySelector('#fname').value;
+  let sname = document.querySelector('#sname').value;
+  
  //Show Alert Message
   document.querySelector('.alert').style.display = 'block';
+
+  //Form Reset After Submission
+  document.getElementById('form1').reset();
+
 
   //Hide Alert Message After Seven Seconds
   setTimeout(function() {document.querySelector('.alert').style.display = 'none';
   }, 7000);
 
-  //listen for submit event//
-document.getElementById('form1').addEventListener('submit', formSubmit);
