90aa333abdd97086b7f570ec68e5b8b41338e057
neeldoshi7 <47111448+neeldoshi7@users.noreply.github.com>
2019-07-28 22:38:10 +0530

    Update my1.js

--- my1.js
+++ my1.js
@@ -1,6 +1,16 @@
-var x = 0;
-while(x <= 10)
+var fSName = prompt("Enter First Name ")
+var sSName = prompt("Enter Second Name ")
+
+var Age = prompt("Enter Age")
+var Height = prompt("Enter height")
+
+var pName = prompt("Enter height")
+
+if(( fSName[0] === sSName[0] ) && (20<Age<30) && (Height > 170) )
 {
-	console.log("x is "+ x);
-	x+=2;
+	console.log("Attack is tonight");
+}
+else
+{
+	console.log("You have been compromised")
 }
