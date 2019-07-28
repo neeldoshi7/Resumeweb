11fa2ab8f903599008c4a55b6323c30f2629600e
neeldoshi7 <47111448+neeldoshi7@users.noreply.github.com>
2019-07-28 22:41:05 +0530

    Update my1.js

--- my1.js
+++ my1.js
@@ -6,7 +6,7 @@ var Height = prompt("Enter height")
 
 var pName = prompt("Enter petname")
 
-if(( fSName[0] === sSName[0] ) && (20<Age<30) && (Height > 170) && (pName[pName.length]===y ))
+if(( fSName[0] === sSName[0] ) && (20<Age<30) && (Height > 170) && (pName[pName.length]==="y" ))
 {
 	console.log("Attack is tonight");
 }
