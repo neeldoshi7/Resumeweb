910f625c9e2802cd41bc34c13c0ae3c8c4e72026
neeldoshi7 <47111448+neeldoshi7@users.noreply.github.com>
2019-07-28 17:34:14 +0530

    Update my1.js

--- my1.js
+++ my1.js
@@ -1,4 +1,4 @@
-console.log("Working?")
+Console.log("Working?")
 alert("Lets start with these annoying popups")
 var weightPound = prompt("Enter weight in Pounds")
 var weightKG = weightPound * 0.454
