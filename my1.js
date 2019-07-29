e297a78532c74d8dcc774ecd099ca18dd641aafe
neeldoshi7 <47111448+neeldoshi7@users.noreply.github.com>
2019-07-29 22:30:35 +0530

    f1

--- my1.js
+++ my1.js
@@ -1,22 +1,10 @@
-var fSName = prompt("Enter First Name ")
-var sSName = prompt("Enter Second Name ")
-
-var Age = prompt("Enter Age")
-var Height = prompt("Enter height")
-
-var pName = prompt("Enter petname")
-
-if((Height > 170))
-	{
-		console.log("working")
-	}
-
-
-if(( fSName[0] === sSName[0] ) && (Age > 20 && Age<30 ) && (Height > 170) && (pName[pName.length - 1]==="y" ))
-{
-	console.log("Attack is tonight");
-}
-else
-{
-	console.log("You have been compromised")
+function sleepIn(weekday, vacation) {
+    if(weekday==false || vacation==true)
+    {
+    	console.log(true);
+    }
+    else
+    {
+    	console.log(false)
+    }
 }
