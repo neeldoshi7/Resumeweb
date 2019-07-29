06c123066c3586d9ea327034d6d037bc8913aa2d
neeldoshi7 <47111448+neeldoshi7@users.noreply.github.com>
2019-07-29 22:44:45 +0530

    Update my1.js

--- my1.js
+++ my1.js
@@ -1,8 +1,8 @@
-console.log("connected2")
+console.log("connected3")
 
 function stringTimes(str, n) {
     for(var i=0;i<n;i++)
     {
-    	console.log(str + console.log(str));
+    	var a=console.log(str)+console.log(str)
     }
 }
