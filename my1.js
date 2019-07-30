e6a0067ec2a69ca5b7747397c4d81cbf7f26d45a
neeldoshi7 <47111448+neeldoshi7@users.noreply.github.com>
2019-07-30 23:15:45 +0530

    Update my1.js

--- my1.js
+++ my1.js
@@ -4,6 +4,15 @@ var roster = []
 function addNew(){
 	var Name = prompt("Add Name")
 	roster.push(Name);
+	console.log(roster);
+}
+
+function RemoveName()
+{
+	console.log("Current roster is "+ roster)
+	var Rname = prompt("Enter Name to be removed");
+	console.log("Element is at "+ roster.indexOf(Rname))
+	delete roster[indexOf(Rname)]
 	console.log(roster)
 }
 
