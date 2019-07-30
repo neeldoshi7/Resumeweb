3d8bb770cced625018db3770db5f783749c5d640
neeldoshi7 <47111448+neeldoshi7@users.noreply.github.com>
2019-07-30 22:51:37 +0530

    3 changes

--- my1.js
+++ my1.js
@@ -1,37 +1,8 @@
-console.log("connected5")
+console.log("connected6")
 
-function caught_speeding(speed, is_birthday){
- 	var ticket = null
- 	if(is_birthday==true)
- 	{
- 		if(speed<=65)
- 		{
- 			return ticket=0
- 		}
- 		else if(speed>65 && speed<=85)
- 		{
- 			return ticket=1
- 		}
- 		else
- 		{
- 			return ticket=2
- 		}
- 	}
- 	else
- 	{
- 		if(speed<=60)
- 		{
- 			return ticket=0
- 		}
- 		else if(speed>60 && speed <=80)
- 		{
- 			return ticket=1
-
- 		}
- 		else
- 		{
- 			return ticket=2
- 		}
- 	} 
- 	console.log(ticket);
+var roster = []
+var Name = prompt("Add Name")
+function addNew(Name){
+	roster.push(Name);
 }
+console.log(roster)
